4a4672cf2d1c3a18de13d5a80928cdeffad63b94
Esmeralda Romero <esmeralda.r.a@hotmail.com>
2022-09-12 15:37:44 -0400

    getting layout started

---
+++ controllers/home.js
@@ -0,0 +1,38 @@
+const router = require("express").Router();
+const { Posts, Comments, User } = require("../models");
+
+// GET all post for homepage
+router.get("/", async (req, res) => {
+  try {
+    const dbPostData = await Posts.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+    console.log(posts);
+
+    res.render("homepage", {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS habit_db;
+CREATE DATABASE habit_db;
+
+USE habit_db;
+
+CREATE TABLE users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    user_password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE posts (
+    post_context TEXT NOT NULL,
+    post_title VARCHAR(255) NOT NULL,
+    post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    userid INT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    FOREIGN KEY(userid) REFERENCES users(id)
+);
+
+CREATE TABLE comments (
+    comment_context TEXT NOT NULL,
+    comment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    userid INT,
+    post_id INT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    FOREIGN KEY(userid) REFERENCES users(id),
+    FOREIGN KEY(post_id) REFERENCES posts(id)
+);
