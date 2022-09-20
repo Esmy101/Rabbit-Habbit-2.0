e6ed7b09d4a418f871aeb5ea38e7b4e25eef55fc
Esmeralda Romero <esmeralda.r.a@hotmail.com>
2022-09-19 20:37:09 -0400

    added extra dependencies and missing source code

---
+++ models/Posts.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Posts extends Model {}
+
+Posts.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userid: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    post_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    post_context: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    post_title: {
+      type: DataTypes.CHAR,
+      allowNull: false,
+    },
+  },
+  {
+    hooks: {},
+    sequelize,
+    createdAt: "post_date",
+    updatedAt: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "posts",
+  }
+);
+
+module.exports = Posts;
